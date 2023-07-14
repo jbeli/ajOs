24803da922c3c8d1ad8505c6e49df6c48c3ba264
ahmed <jbeli90.ahmed@gmail.com>
2023-07-14 23:40:28 +0200

    create ignorefile

--- os/osTask.c
+++ os/osTask.c
@@ -3,24 +3,26 @@
 StatusType ActivateTask(TaskType TaskID)
 {
 	
-	
+	return 0 ;
 }
 
 StatusType TerminateTask(void)
 {
+	return 0 ;
 }
 
 StatusType ChainTask(TaskType TaskID)
 {
+	return 0 ;
 }
 
 StatusType GetTaskID(TaskRefType TaskID)
 {
-	
+	return 0 ;
 }
 
 StatusType GetTaskState(TaskType TaskID, TaskStateRefType State)
 {
-	
+	return 0 ;
 }
 
